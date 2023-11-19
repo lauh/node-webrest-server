b5a3883be38587a1af9d37888d3001423e8078cb
lauh <laurherant@gmail.com>
2023-11-19 17:31:27 +0100

    fix port number

--- src/presentation/server.ts
+++ src/presentation/server.ts
@@ -36,7 +36,7 @@ export class Server {
     })
 
     this.app.listen(3000, () => {
-      console.log(`Server running on Port ${this.port}`);
+      console.log(`Server running on Port ${Number(this.port)}`);
       
     });
   }
